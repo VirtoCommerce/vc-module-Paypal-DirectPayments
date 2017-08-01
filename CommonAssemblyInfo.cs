d24dc7525323f5be88c6026d8ec3c7e6c023f72c
Aleksandr Vishnyakov <av@virtoway.com>
2017-08-01 19:52:11 +0500

    [publish] 2.12.1

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="GlobalAssemblyInfo.cs" company="VirtoCommerce">
+//   Copyright © VirtoCommerce. All rights reserved.
+// </copyright>
+// <summary>
+//   Virto Commerce
+// </summary>
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("VirtoCommerce")]
+[assembly: AssemblyProduct("Virto Commerce Paypal.DirectPayments Module")]
+[assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2017")]
+
+[assembly: AssemblyFileVersion("2.12.1.0")]
+[assembly: AssemblyVersion("2.12.1.0")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
